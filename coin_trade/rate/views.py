1627126b9ff82f2f4446cfc762c2645126fb0742
hhironori <hironori7117@gmail.com>
2020-03-28 21:10:30 +0900

    Slack自動通知

--- coin_trade/rate/views.py
+++ coin_trade/rate/views.py
@@ -7,7 +7,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
 import json
-
+import os
+import slack
+import time
 # Create your views here.
 
 def index(request):
@@ -16,10 +18,61 @@ def index(request):
 
     URL = 'https://coincheck.com/api/rate/'
 
-    dispList = []
+    # Slack 通知の処理
+    url = "https://slack.com/api/chat.postMessage"
+    data = {
+    "token": "xoxb-987250221396-1012588986674-535g1TLN2MvO4h9xvMR1qSKg",
+    "channel": "CV17C6MC4",
+    "text": '計測開始'
+    }
+    requests.post(url, data=data)
+ 
+    # 1分間ごとにチェックする
+    time_count = 0 #時間計測(s)
+    limit = 180 #動かす最大時間(s)
+    difference = 0 #前のスパンからの差分
+    rate_previous = 0 #前のスパンの値
+    rate_now = 0 #取得した値
 
-    for key, item in coins.items():
-        coincheck = (key,requests.get(URL+item).json())
-        dispList.append(coincheck)
+    while time_count < limit:
+        #BTCの値を取得
+        rate_now = int(requests.get(URL+'btc_jpy').json()["rate"][:-2])
+        #前回との値の差分を抽出
+        if rate_previous != 0: 
+            difference = rate_now - rate_previous
+        #通知文作成
+        text = 'BTC：' + str(rate_now) + ' ' + '前スパンより' + str(difference) 
+        # Slack 通知の処理
+        url = "https://slack.com/api/chat.postMessage"
+        data = {
+        "token": "xoxb-987250221396-1012588986674-535g1TLN2MvO4h9xvMR1qSKg",
+        "channel": "CV17C6MC4",
+        "text": text
+        }
+        requests.post(url, data=data)
+        #取得した値を保存
+        rate_previous = rate_now
+        #処理を一定時間停止(一定時間ごとに起動)
+        time.sleep(20)
+        #累計計測時間を調整
+        time_count += 20
+        #計測時間を超えた場合、処理中止
+        if time_count > limit:
+            break
+    
+    # Slack 通知の処理
+    url = "https://slack.com/api/chat.postMessage"
+    data = {
+    "token": "xoxb-987250221396-1012588986674-535g1TLN2MvO4h9xvMR1qSKg",
+    "channel": "CV17C6MC4",
+    "text": '計測終了'
+    }
+    requests.post(url, data=data)
+
+    # 画面表示に使う
+    dispList = []
+    # for key, item in coins.items():
+    #     coincheck = (key,requests.get(URL+item).json())
+    #     dispList.append(coincheck)
 
     return JsonResponse(dispList, safe=False)
